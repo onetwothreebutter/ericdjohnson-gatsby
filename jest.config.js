5797c2e6181ad1ea53763e893003a27bb1da77e6
Eric Johnson <eric.d.johnson@gmail.com>
2018-02-25 15:25:10 -0600

    adding jest config, jest, and first test

---
+++ jest.config.js
@@ -0,0 +1,5 @@
+// jest.config.js
+module.exports = {
+    verbose: true,
+    modulePathIgnorePatterns: ["/node_modules/", "<rootDir>/\.cache/", "<rootDir>/public/"]
+}
---
+++ src/pages/__tests__/index.js
@@ -0,0 +1,6 @@
+import IndexPage from '../index'
+
+test('It workss', () => {
+    const x = '';
+    expect(2).toBe(2);
+})
