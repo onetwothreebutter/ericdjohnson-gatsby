9690b6504689633e9c17720d7553779ce22359fe
Eric Johnson <ejohnson@csod.com>
2020-10-25 16:18:02 -0500

    split into /frontend and /backend and init Sanity for blog

--- frontend/src/__tests__/index.js
+++ frontend/src/__tests__/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import shallowToJson from 'enzyme-to-json'
 
-import IndexPage from '../pages/index'
+import IndexPage from '../pages'
 
 test('Index Page should render correctly', () => {
   const wrapper = shallow(<IndexPage />)
--- frontend/src/__tests__/work-ive-done/index.js
+++ frontend/src/__tests__/work-ive-done/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import shallowToJson from 'enzyme-to-json'
 
-import WorkIveDonePage from '../../pages/work-ive-done/index'
+import WorkIveDonePage from '../../pages/work-ive-done'
 
 test('Work Ive Done Page should render correctly', () => {
   const wrapper = shallow(<WorkIveDonePage />)
